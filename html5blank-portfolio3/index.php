e65cf6e95cb04802c0efcba7d758ab1ba9bd8042
Janice <janicen@sfu.ca>
2013-07-07 01:49:01 -0700

    Portfolio3: Started styling

---
+++ html5blank-portfolio3/index.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+	
+	<!-- section -->
+	<section role="main">
+
+					<!-- graphics -->
+
+<!-- 						<a href="<?php echo home_url(); ?>"> -->
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							
+								<h1>Graphics</h1>
+							
+
+							<div class="column1">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/graphicdesign_1.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column2">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/graphicdesign_2.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column3">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/spatialdesign.png" alt="project" class="project-img">
+							</div></a>
+						<!-- </a> -->
+			
+					<!-- /graphics -->
+
+					<!-- interactions -->
+		
+						
+								<h1>Interactions</h1>
+					
+
+<!-- 						<a href="<?php echo home_url(); ?>"> -->
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<div class="column1">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/interfacedesign_1.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column2">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/interfacedesign_2.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column3">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/interactiondesign_3.png" alt="project" class="project-img">
+							</div></a>
+						<!-- </a> -->
+			
+					<!-- /interactions -->
+
+					<!-- products -->
+			
+			
+								<h1>Products</h1>
+					
+
+<!-- 						<a href="<?php echo home_url(); ?>"> -->
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<div class="column1">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/productdesign_1.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column2">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/productdesign_2.png" alt="project" class="project-img">
+							</div></a>
+							<div class="column3">
+							<a href="http://localhost:8888/wordpress/?page_id=29"><img src="<?php echo get_template_directory_uri(); ?>/img/productdesign_3.png" alt="project" class="project-img">
+							</div></a>
+						<!-- </a> -->
+				
+					<!-- /products -->
+	
+	</section>
+	<!-- /section -->
+
+<?php get_footer(); ?>
